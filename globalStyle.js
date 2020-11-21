beaa9dfa783fc8f014cc2cc8188418de5ef9faa8
Isabelle-123 <isacas031@gmail.com>
2020-11-21 20:41:57 +0100

    cleaning

--- globalStyle.js
+++ globalStyle.js
@@ -1,16 +1,11 @@
 import { Dimensions } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 
 //Guideline sizes are based on standard ~5" screen mobile device
-const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = size => width / guidelineBaseWidth * size;
 const verticalScale = size => height / guidelineBaseHeight * size;
-const moderateScale = (size, factor = 0.5) => size + ( scale(size) - size ) * factor;
-
-//https://medium.com/soluto-engineering/size-matters-5aeeb462900a
 
 export const text = {
   mini: {
